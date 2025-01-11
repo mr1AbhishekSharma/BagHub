f81afb6e28e48579d6035f5ebdd2cbc595cd3b48
Abhishek Sharma <mr1abhisheksharma@gmail.com>
2025-01-12 00:30:40 +0530

    created models

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require('express');
+const app=express();
+
+const cookieParser=require('cookie-parser');
+const path=require('path');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,"public")));
+app.set("view engine","ejs");
+
+app.get('/',function(req,res){
+    res.send("hey");
+})
+app.listen(3000);
