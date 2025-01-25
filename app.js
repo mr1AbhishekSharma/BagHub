536c7f1ae5e156773f6b6a2062dc824c08933f03
Abhishek Sharma <mr1abhisheksharma@gmail.com>
2025-01-25 14:21:14 +0530

    done

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app=express();
 const cookieParser=require('cookie-parser');
 const path=require('path');
 const db=require('./config/mongoose-connection');
-const ownersRouter=require('./routes/ownersRouter')
-const usersRouter=require('./routes/usersRouter')
-const productsRouter=require('./routes/productsRouter')
+const ownersRouter=require('./routes/ownersRouter');
+const usersRouter=require('./routes/usersRouter');
+const productsRouter=require('./routes/productsRouter');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
