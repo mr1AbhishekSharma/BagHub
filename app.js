40a73f08379a5cf1f3ccad336f6beeb3437ee949
Abhishek Sharma <mr1abhisheksharma@gmail.com>
2025-01-27 01:07:50 +0530

    made login sign up routes

--- app.js
+++ app.js
@@ -7,17 +7,19 @@ const db=require('./config/mongoose-connection');
 const ownersRouter=require('./routes/ownersRouter');
 const usersRouter=require('./routes/usersRouter');
 const productsRouter=require('./routes/productsRouter');
+const indexRouter=require('./routes/index');
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 app.set("view engine","ejs");
 
+
+app.use("/",indexRouter);
 app.use("/owners",ownersRouter);
 app.use("/users",usersRouter);
 app.use("/products",productsRouter);
 
-app.get('/',function(req,res){
-    res.send("hey");
-})
+
 app.listen(3000);
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -7,7 +7,7 @@ if(process.env.NODE_ENV==="development"){
     router.post("/create", async function(req,res){
         let owners=await ownerModel.find();
         if(owners.length>0){
-            return res.send(503).send("you don't have access to create a new owner");
+            return res.status(500).send("you don't have access to create a new owner");
         }
         let{fullname,email,password}=req.body;
         let createdOwner=await ownerModel.create({
