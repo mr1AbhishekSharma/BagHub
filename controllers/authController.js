7971f4ff60e530c864cc2c0f71c72257fb689945
Abhishek Sharma <mr1abhisheksharma@gmail.com>
2025-01-28 16:24:08 +0530

    new commit on 28-01-2025

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,17 @@
 const userModel=require('../models/user-model')
 const bcrypt=require('bcrypt');
 const jwt=require('jsonwebtoken');
-const generateToken=require("../utils/generateToken.js");
+const { generateToken }=require("../utils/generateToken");
 
 
 
-module.exports.registerUser=function(req,res){
+module.exports.registerUser=async function(req,res){
     try{
         let{email,password,username}=req.body;
+
+        let user= await userModel.findOne({email:email})
+        if(user) return res.status(401).send("You already have an account. Please Login");
+        else{}
         bcrypt.genSalt(10,function(err,salt){
             bcrypt.hash(password,salt, async function(err,hash){
                 if(err) return res.send(err.message);
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const router=express.Router();
+const {registerUser}=require("../controllers/authController");
 
 
 router.get("/",function(req,res){
     res.send("hey its working");
 });
 
-router.post("/register",);
+router.post("/register",registerUser);
 
 module.exports=router;
